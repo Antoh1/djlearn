6713aa74e9dba3a0176adf2d48aff253ae904d4d
Antoh1 <antotiro@gmail.com>
2024-08-15 15:39:53 +0300

    [UPT] added table and styling

--- learnDJ/base/urls.py
+++ learnDJ/base/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("",views.Home, name="home"),
     path("<int:item_id>/",views.Index, name="index"),
+    #path("med/",views.Med, name="med"),
     #path("post/",views.Post, name="posts"),
 ]
--- learnDJ/base/views.py
+++ learnDJ/base/views.py
@@ -6,13 +6,15 @@ from .models import Author, TodoList, Item
 
 def Index(request, item_id):
     #tl = TodoList.objects.get(id=item_id)
-    if Item.objects.filter(id=item_id):
+
+    if Item.objects.filter(todolist=item_id):
+        items = Item.objects.filter(todolist=item_id)
         itm = Item.objects.get(id=item_id)
     #item_id = 1
     #itm = Item.objects.get(id=item_id)
     print(itm)
     #return HttpResponse("<table><th>Without Reference</th><th>Without Reference2</th><tr><td>%s</td><td>  %s</td><td>  %s</td><td>  %s</td><td>  %s</td></tr></table>" %(tl.title, itm.name, itm.description, itm.completed, itm.date))
-    return render(request, 'base/item_list.html',{"tdl": itm.todolist, "iname": itm.name})
+    return render(request, 'base/item_list.html',{"tdl": itm.todolist, "iname": itm.name, "items": items})
 
 def Home(request):
     tl = TodoList.objects.all()
@@ -22,3 +24,6 @@ def Home(request):
         #url += "<h2><a href='/" + str(l.id) + "'>" + l.title + "</a></h2>" + "\n"
     return render(request, 'base/home.html',{"lnk": tl})
     #return HttpResponse(url)
+
+# def Med(request):
+#     return render(request, 'base/index.html')
